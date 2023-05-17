39c4c2442587892f7994920352b9e542aa58f051
Michał Siedlecki <msiedlecki@intel471.com>
2023-05-17 13:02:03 +0200

    added post-detail page and simple tag

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -11,21 +11,31 @@ register = template.Library()
 def author_details(author, current_user):
     if not isinstance(author, user_model):
         # return empty string as safe default
-        return ""
+        return ''
 
     if author == current_user:
-        return format_html("<strong>me</strong>")
+        return format_html('<strong>me</strong>')
 
     if author.first_name and author.last_name:
-        name = f"{author.first_name} {author.last_name}"
+        name = f'{author.first_name} {author.last_name}'
     else:
-        name = f"{author.username}"
+        name = f'{author.username}'
 
     if author.email:
         prefix = format_html('<a href="mailto:{}">', author.email)
-        suffix = format_html("</a>")
+        suffix = format_html('</a>')
     else:
-        prefix = ""
-        suffix = ""
+        prefix = ''
+        suffix = ''
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+
+@register.simple_tag
+def row():
+    return format_html('<div class="row">')
+
+
+@register.simple_tag
+def endrow():
+    return format_html('</div>')
